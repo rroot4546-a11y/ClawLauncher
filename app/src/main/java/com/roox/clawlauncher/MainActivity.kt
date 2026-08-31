77126f61c8130725f86fce054e223fc38f2ef2dc
rroot4546-a11y <rroot4546@gmail.com>
2026-08-31 20:30:19 +0000

    Fix unbalanced braces in MainActivity

--- app/src/main/java/com/roox/clawlauncher/MainActivity.kt
+++ app/src/main/java/com/roox/clawlauncher/MainActivity.kt
@@ -170,7 +170,6 @@ class MainActivity : ComponentActivity() {
                             },
                             onBack = { selectedTab = 0 }
                         )
-                        }
                     }
                 }
             }
